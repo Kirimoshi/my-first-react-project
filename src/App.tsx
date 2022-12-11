200f542ab526f5049d6ef59ea84143970a09f6eb
Kirimoshi <baronaxaro@gmail.com>
2022-12-11 19:31:01 +0200

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Product} from "./components/Product";
 import {useProducts} from "./hooks/products";
 import {Loading} from "./components/Loading";
 import {Error} from "./components/Error";
+import {Modal} from "./components/Modal";
+import {CreateProduct} from "./components/CreateProduct";
 
 function App() {
     const { products, loading, error } = useProducts();
+    const [modal, setModal] = useState(true);
 
     return (
         <div className='container mx-auto max-w-2xl pt-5'>
             { loading && <Loading /> }
             { error && <Error error = { error } /> }
             { products.map(product => <Product product={ product } key={ product.id } />) }
+
+            {modal && <Modal title="Create new product">
+                <CreateProduct onCreate={ () => setModal(false) } />
+            </Modal>}
         </div>
     )
 }
